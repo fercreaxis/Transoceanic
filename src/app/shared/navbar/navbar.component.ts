6c53217416011575f7a39f9c713de61e0e300cd5
chelaruc <chelaru_ciprian@yahoo.com>
2017-05-29 18:32:10 +0300

    Fixed sidebar mini

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit, Renderer, ViewChild, ElementRef, Directive } from '@
 import { ROUTES } from '../.././sidebar/sidebar-routes.config';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
-
+var misc:any ={
+    navbar_menu_visible: 0,
+    active_collapse: true,
+    disabled_collapse_init: 0,
+}
 declare var $: any;
 @Component({
     moduleId: module.id,
@@ -30,6 +34,34 @@ export class NavbarComponent implements OnInit{
 
         var navbar : HTMLElement = this.element.nativeElement;
         this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
+        if($('body').hasClass('sidebar-mini')){
+            misc.sidebar_mini_active = true;
+        }
+        $('#minimizeSidebar').click(function(){
+            var $btn = $(this);
+
+            if(misc.sidebar_mini_active == true){
+                $('body').removeClass('sidebar-mini');
+                misc.sidebar_mini_active = false;
+
+            }else{
+                setTimeout(function(){
+                    $('body').addClass('sidebar-mini');
+
+                    misc.sidebar_mini_active = true;
+                },300);
+            }
+
+            // we simulate the window Resize so the charts will get updated in realtime.
+            var simulateWindowResize = setInterval(function(){
+                window.dispatchEvent(new Event('resize'));
+            },180);
+
+            // we stop the simulation of Window Resize after the animations are completed
+            setTimeout(function(){
+                clearInterval(simulateWindowResize);
+            },1000);
+        });
     }
     isMobileMenu(){
         if($(window).width() < 991){
