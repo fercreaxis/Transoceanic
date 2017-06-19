ac70120652fb96acd0d52de52392df047d229aab
chelaruc <chelaru_ciprian@yahoo.com>
2017-06-19 15:54:17 +0300

    Fixed datetimepicker

--- src/app/forms/extendedforms/extendedforms.component.ts
+++ src/app/forms/extendedforms/extendedforms.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import initSliders = require('../../../../assets/js/init/initSliders.js');
-// import initDatetimepickers = require('../../../../assets/js/init/initDatetimepickers.js');
+
 
 declare var require: any
 
@@ -14,17 +13,18 @@ declare var $:any;
 
 export class ExtendedFormsComponent implements OnInit{
     ngOnInit() {
-        // $.getScript('../../../assets/js/plugins/bootstrap-datetimepicker.js');
-        // $.getScript('../../../assets/js/plugins/jquery.tagsinput.js');
-        //
-        // // Init Tags Input
+
+        // Init Tags Input
         if($(".tagsinput").length != 0){
             $(".tagsinput").tagsinput();
         }
 
         //  Init Bootstrap Select Picker
         if($(".selectpicker").length != 0){
-            $(".selectpicker").selectpicker();
+            $(".selectpicker").selectpicker({
+                iconBase: "fa",
+                tickIcon: "fa-check"
+            });
         }
 
         $('.datetimepicker').datetimepicker({
@@ -52,8 +52,7 @@ export class ExtendedFormsComponent implements OnInit{
                 next: 'fa fa-chevron-right',
                 today: 'fa fa-screenshot',
                 clear: 'fa fa-trash',
-                close: 'fa fa-remove',
-                inline: true
+                close: 'fa fa-remove'
             }
          });
 
@@ -69,9 +68,7 @@ export class ExtendedFormsComponent implements OnInit{
                 next: 'fa fa-chevron-right',
                 today: 'fa fa-screenshot',
                 clear: 'fa fa-trash',
-                close: 'fa fa-remove',
-                inline: true
-
+                close: 'fa fa-remove'
             }
          });
 
