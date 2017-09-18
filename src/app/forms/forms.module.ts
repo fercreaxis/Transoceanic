58257ac7e4f5e34dbfa7d74b912f814fad7d4b51
chelaruc <chelaru_ciprian@yahoo.com>
2017-09-18 18:20:21 +0300

    Fixed date picker

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -5,8 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { TagInputModule } from 'ngx-chips';
 import { SelectModule } from 'ng2-select';
-import { MdDatepickerModule, MdInputModule, MdNativeDateModule, MdSelectModule } from '@angular/material';
-import { DatepickerModule } from 'angular2-material-datepicker'
+import {  MaterialModule, MdDatepickerModule, MdNativeDateModule, MdInputModule, MdSelectModule } from '@angular/material';
 
 import { FormsRoutes } from './forms.routing';
 
@@ -24,11 +23,11 @@ import { WizardComponent } from './wizard/wizard.component';
     ReactiveFormsModule,
     NouisliderModule,
     TagInputModule,
+    MaterialModule,
     MdDatepickerModule,
     MdInputModule,
     MdNativeDateModule,
-    MdSelectModule,
-    DatepickerModule
+    MdSelectModule
   ],
   declarations: [
       ExtendedFormsComponent,
