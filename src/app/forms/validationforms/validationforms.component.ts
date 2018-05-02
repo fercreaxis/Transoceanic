ffe86fe4b47a825a9a98d885be3160f32f167f71
chelaruc <chelaru_ciprian@yahoo.com>
2018-05-02 16:37:36 +0300

    fixed validation forms

--- src/app/forms/validationforms/validationforms.component.ts
+++ src/app/forms/validationforms/validationforms.component.ts
@@ -24,6 +24,21 @@ export class ValidationFormsComponent {
     Validators.email,
   ]);
 
+  validEmailRegister: boolean = false;
+  validConfirmPasswordRegister: boolean = false;
+  validPasswordRegister: boolean = false;
+
+  validEmailLogin: boolean = false;
+  validPasswordLogin: boolean = false;
+
+  validTextType: boolean = false;
+  validEmailType: boolean = false;
+  validNumberType: boolean = false;
+  validUrlType: boolean = false;
+  pattern = "https?://.+";
+  validSourceType: boolean = false;
+  validDestinationType: boolean = false;
+
   matcher = new MyErrorStateMatcher();
   register : FormGroup;
   login : FormGroup;
@@ -81,10 +96,9 @@ export class ValidationFormsComponent {
       this.register = this.formBuilder.group({
         // To add a validator, we must first convert the string value into an array. The first item in the array is the default value if any, then the next item in the array is the validator. Here we are adding a required validator meaning that the firstName attribute must have a value in it.
         email: [null, [Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
-// email: '',
         // We can use more than one validator per field. If we want to use more than one validator we have to wrap our array of validators with a Validators.compose function. Here we are using a required, minimum length and maximum length validator.
         optionsCheckboxes: ['', Validators.required],
-        password: ['', Validators.required],
+        password: ['',  Validators.compose([Validators.required, Validators.minLength(6)])],
         confirmPassword: ['', Validators.required],
        }, {
          validator: PasswordValidation.MatchPassword // your validation method
@@ -99,8 +113,8 @@ export class ValidationFormsComponent {
          // To add a validator, we must first convert the string value into an array. The first item in the array is the default value if any, then the next item in the array is the validator. Here we are adding a required validator meaning that the firstName attribute must have a value in it.
          text: [null, Validators.required],
          email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
-         number: [null , Validators.required],
-         url: [null, Validators.required],
+         number: [null, Validators.required],
+         url: [null , Validators.required],
          // We can use more than one validator per field. If we want to use more than one validator we have to wrap our array of validators with a Validators.compose function. Here we are using a required, minimum length and maximum length validator.
          password: ['', Validators.required],
          confirmPassword: ['', Validators.required],
@@ -108,4 +122,90 @@ export class ValidationFormsComponent {
           validator: PasswordValidation.MatchPassword // your validation method
       });
   }
+
+  emailValidationRegister(e){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(e).toLowerCase())) {
+        this.validEmailRegister = true;
+    } else {
+      this.validEmailRegister = false;
+    }
+  }
+  passwordValidationRegister(e){
+      if (e.length > 5) {
+          this.validPasswordRegister = true;
+      }else{
+        this.validPasswordRegister = false;
+      }
+  }
+  confirmPasswordValidationRegister(e){
+    if (this.register.controls['password'].value === e) {
+        this.validConfirmPasswordRegister = true;
+    }else{
+      this.validConfirmPasswordRegister = false;
+    }
+  }
+
+  emailValidationLogin(e){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(e).toLowerCase())) {
+        this.validEmailLogin= true;
+    } else {
+      this.validEmailLogin = false;
+    }
+  }
+  passwordValidationLogin(e){
+      if (e.length > 5) {
+          this.validPasswordLogin = true;
+      }else{
+        this.validPasswordLogin = false;
+      }
+  }
+
+
+  textValidationType(e){
+      if (e) {
+          this.validTextType = true;
+      }else{
+        this.validTextType = false;
+      }
+  }
+  emailValidationType(e){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(e).toLowerCase())) {
+        this.validEmailType = true;
+    } else {
+      this.validEmailType = false;
+    }
+  }
+  numberValidationType(e){
+      if (e) {
+          this.validNumberType = true;
+      }else{
+        this.validNumberType = false;
+      }
+  }
+  urlValidationType(e){
+    try {
+      new URL(e);
+      this.validUrlType = true;
+    } catch (_) {
+      this.validUrlType = false;
+    }
+  }
+  sourceValidationType(e){
+      if (e) {
+          this.validSourceType = true;
+      }else{
+        this.validSourceType = false;
+      }
+  }
+  confirmDestinationValidationType(e){
+    if (this.type.controls['password'].value === e) {
+        this.validDestinationType = true;
+    }else{
+      this.validDestinationType = false;
+    }
+  }
+
 }
